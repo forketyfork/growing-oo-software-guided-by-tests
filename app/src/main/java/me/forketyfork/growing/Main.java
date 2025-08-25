86abc603460535aa59786ef60dfb3ff2f1687b2a
Forketyfork <forketyfork@icloud.com>
2025-08-25 10:29:19 +0200

    Added MainWindow and Swing initialization

--- app/src/main/java/me/forketyfork/growing/Main.java
+++ app/src/main/java/me/forketyfork/growing/Main.java
@@ -1,7 +1,22 @@
 package me.forketyfork.growing;
 
+import me.forketyfork.growing.auctionsniper.ui.MainWindow;
+
+import javax.swing.*;
+
 public class Main {
-    public static void main(String... args) {
-        System.out.println("Hello, World!");
+
+    private MainWindow ui;
+
+    public Main() throws Exception {
+        startUserInterface();
+    }
+
+    public static void main(String... args) throws Exception {
+        Main main = new Main();
+    }
+
+    private void startUserInterface() throws Exception {
+        SwingUtilities.invokeAndWait((Runnable) () -> ui = new MainWindow());
     }
 }
